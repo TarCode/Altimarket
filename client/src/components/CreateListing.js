e4d55061bdb0143ea56b6c45bd00b3e479c48f23
TarCode <tarcode33@gmail.com>
2019-04-19 22:26:11 +0200

    updated cloudinary url

--- client/src/components/CreateListing.js
+++ client/src/components/CreateListing.js
@@ -17,7 +17,7 @@ export default class CreateListing extends Component {
 
   uploadImageToCloudinaryAndSubmitData = async () => {
     this.setState({ loading: true })
-    var cloudinary_url = 'https://api.cloudinary.com/v1_1/blocktame/image/upload?upload_preset=nudge-uploads';
+    var cloudinary_url = 'https://api.cloudinary.com/v1_1/blocktame/image/upload?upload_preset=eth-hack';
 
     let formData = new FormData();
     
@@ -36,59 +36,65 @@ export default class CreateListing extends Component {
   }
 
   render() {
-    const {name , description, image_url, price_in_wei} = this.state;
+    const {name , description, loading, price_in_wei} = this.state;
     return (
       <div>
-        <h2>Create Listing</h2>
-        <input onChange={this.onChange} placeholder="Name" name='name' type='text' value={name}/>
-        <input onChange={this.onChange} placeholder="Description" name='description' type='text' value={description}/>
-        <div style={{
-            position: 'relative'
-        }}>
-            <h5>Upload image</h5>
-            <input style={{ 
-                position: 'absolute', 
-                width: '100%', 
-                top: 0, left: 0, 
-                right: 0, bottom: 0, 
-                zIndex:9999,
-                cursor: 'pointer'
-            }} onChange={(e) => {
-                var file = e.target.files[0];
+        {
+            loading ?
+            <p>Loading bru...</p> :
+            <div>
+                <h2>Create Listing</h2>
+                <input onChange={this.onChange} placeholder="Name" name='name' type='text' value={name}/>
+                <input onChange={this.onChange} placeholder="Description" name='description' type='text' value={description}/>
+                <div style={{
+                    position: 'relative'
+                }}>
+                    <h5>Upload image</h5>
+                    <input style={{ 
+                        position: 'absolute', 
+                        width: '100%', 
+                        top: 0, left: 0, 
+                        right: 0, bottom: 0, 
+                        zIndex:9999,
+                        cursor: 'pointer'
+                    }} onChange={(e) => {
+                        var file = e.target.files[0];
 
-                let f = file && window.URL.createObjectURL(file);
+                        let f = file && window.URL.createObjectURL(file);
 
-                var reader  = new FileReader();
+                        var reader  = new FileReader();
 
-                reader.addEventListener("load", function () {
-                    this.setState({ preview: f, image_url: file, changeImg: false })
-                }.bind(this), false);
+                        reader.addEventListener("load", function () {
+                            this.setState({ preview: f, image_url: file, changeImg: false })
+                        }.bind(this), false);
 
-                if (file) {
-                    reader.readAsDataURL(file);
-                }
-            }} type="file" />
-        </div>
-        <div style={{
-            position: 'relative'
-        }}>
-            {
-                this.state.preview ?
-                <img style={{
-                    position: 'relative',
-                    top: 0,
-                    left: 0,
-                    right: 0,
-                    width: '100%',
-                    height: '100%',
-                    boxSizing: 'border-box',
-                    objectFit: 'cover'
-                }} src={this.state.preview} alt="" /> :
-                null
-            }
-        </div>
-        <input onChange={this.onChange} placeholder="Price" name='price_in_wei' value={price_in_wei}/>
-        <button onClick={this.submit}>Add</button>
+                        if (file) {
+                            reader.readAsDataURL(file);
+                        }
+                    }} type="file" />
+                </div>
+                <div style={{
+                    position: 'relative'
+                }}>
+                    {
+                        this.state.preview ?
+                        <img style={{
+                            position: 'relative',
+                            top: 0,
+                            left: 0,
+                            right: 0,
+                            width: '100%',
+                            height: '100%',
+                            boxSizing: 'border-box',
+                            objectFit: 'cover'
+                        }} src={this.state.preview} alt="" /> :
+                        null
+                    }
+                </div>
+                <input onChange={this.onChange} placeholder="Price" name='price_in_wei' value={price_in_wei}/>
+                <button onClick={this.uploadImageToCloudinaryAndSubmitData}>Add</button>
+            </div>
+        }
       </div>
     )
   }
