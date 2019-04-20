c2188486b3101cd468d8d7733334c94e064a3e40
TarCode <tarcode33@gmail.com>
2019-04-20 08:45:46 +0200

    added chat message getter methods

--- contracts/Chat.sol
+++ contracts/Chat.sol
@@ -16,5 +16,29 @@ contract Chat is Market {
 
     mapping(uint => Message[]) listingIdToMessages;
 
+    function getListingMessageCount(uint _listingId) external view returns(uint) {
+        return listingIdToMessages[_listingId].length;
+    }
+
+    function sendMessage(string calldata _msg, uint _listingId) external {
+        Message memory message = Message(_msg, msg.sender, block.timestamp);
+        listingIdToMessages[_listingId].push(message);
+        emit NewMessage(_msg, msg.sender, block.timestamp, _listingId);
+    }
+
+    function getListingMessageTextByIndex(uint _listingId, uint _index) external view returns(string memory) {
+        Message memory message = listingIdToMessages[_listingId][_index];
+        return(message.message);
+    }
+
+    function getListingMessageSenderByIndex(uint _listingId, uint _index) external view returns(address) {
+        Message memory message = listingIdToMessages[_listingId][_index];
+        return(message.user);
+    }
+
+    function getListingMessageTimestampByIndex(uint _listingId, uint _index) external view returns(uint) {
+        Message memory message = listingIdToMessages[_listingId][_index];
+        return(message.timestamp);
+    }
 
 }
