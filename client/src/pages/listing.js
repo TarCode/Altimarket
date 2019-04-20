4e428ceb476e06afa275298411713320240de4e7
TarCode <tarcode33@gmail.com>
2019-04-20 09:23:46 +0200

    migrated contracts, added chat component to single listing view

--- client/src/pages/listing.js
+++ client/src/pages/listing.js
@@ -12,13 +12,14 @@ export default class extends Component {
         this.setState({ loading: true })
         const message_count = await chat_contract.methods.getListingMessageCount(id).call();
 
-
+        console.log("MESSAGE COUNT", message_count);
+        
         this.setState({ loading: false, message_count })
     }
 
   render() {
     const { name, description, image_id, price_in_wei } = this.props;
-    const { loading } = this.state;
+    const { loading, message_count } = this.state;
     return (
       <div>
         {
@@ -38,7 +39,24 @@ export default class extends Component {
                     <p>{description}</p>
                     <h3>{price_in_wei}</h3>
                     <button>Buy</button>
-                    <button>Chat</button>
+                    <button>Chat {message_count.toString()}</button>
+                </div>
+                <button className="open-button" onClick={() => {
+                    document.getElementById("myForm").style.display = "block";
+                }}>Chat</button>
+
+                <div className="chat-popup" id="myForm">
+                <form action="/action_page.php" className="form-container">
+                    <h1>Chat</h1>
+
+                    <label for="msg"><b>Message</b></label>
+                    <textarea className='msg-box' placeholder="Type message.." name="msg" required></textarea>
+
+                    <button type="submit" className="btn">Send</button>
+                    <button type="button" className="btn cancel" onClick={() => {
+                        document.getElementById("myForm").style.display = "none";
+                    }}>Close</button>
+                </form>
                 </div>
             </div>
         }
